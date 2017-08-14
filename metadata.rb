6518c9c070eea04411d4e070ab0452e8f4a91006
Mihai Petracovici <m-petracovici@wiu.edu>
2017-08-14 14:57:32 -0500

    Add install action to node recipe

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'surrender@evilmartians.com'
 license          'All rights reserved'
 description      'Installs/Configures Prometheus Exporters'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.6'
+version          '0.1.7'
 
 supports 'ubuntu'
 supports 'centos', '>= 6.9'
