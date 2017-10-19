30a76da4dc541880dc634b0b6b31de6bb2d68df1
Viktor Radnai <viktor.radnai@ticketmaster.co.uk>
2017-10-19 01:03:20 +0100

    Added checksum for SNMP exporter

--- attributes/snmp.rb
+++ attributes/snmp.rb
@@ -1,3 +1,3 @@
 default['prometheus_exporters']['snmp']['version'] = '0.4.0'
 default['prometheus_exporters']['snmp']['url'] = "https://github.com/prometheus/snmp_exporter/releases/download/v#{node['prometheus_exporters']['snmp']['version']}/snmp_exporter-#{node['prometheus_exporters']['snmp']['version']}.linux-amd64.tar.gz"
-# default['prometheus_exporters']['snmp']['checksum'] = ''
+default['prometheus_exporters']['snmp']['checksum'] = '49c93ffb0385894aed2e1d7a20758191f3ba76bfc1c46bc46adab3637303053e'
