0bb4a5aa35f20c1529855893dbfe528d0ca2ecd8
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2019-05-20 15:55:52 +0200

    Update statds exporter version to stopgap the rather critical https://github.com/prometheus/statsd_exporter/issues/220

--- attributes/statsd.rb
+++ attributes/statsd.rb
@@ -1,6 +1,6 @@
-default['prometheus_exporters']['statsd']['version'] = '0.10.3'
+default['prometheus_exporters']['statsd']['version'] = '0.10.4'
 default['prometheus_exporters']['statsd']['url'] = "https://github.com/prometheus/statsd_exporter/releases/download/v#{node['prometheus_exporters']['statsd']['version']}/statsd_exporter-#{node['prometheus_exporters']['statsd']['version']}.linux-amd64.tar.gz"
-default['prometheus_exporters']['statsd']['checksum'] = 'e0c237f22693690025d52ee2beac25ca4d7bd0c953ff9e785b6e2c685d5f78d0'
+default['prometheus_exporters']['statsd']['checksum'] = 'a2ff6d60a09eac103371d59caf849f62e2323df29868ee9a4c254d76551b2cef'
 
 default['prometheus_exporters']['statsd']['port'] = 9102
 default['prometheus_exporters']['statsd']['user'] = 'root'
