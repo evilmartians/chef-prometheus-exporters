9f680a988d3ada73f0c3498073a9978871c1095b
Stephan Scheying <stephan@scheying.de>
2019-08-24 11:05:00 +0200

    Set default user attribute for blackbox exporter

--- attributes/blackbox.rb
+++ attributes/blackbox.rb
@@ -3,3 +3,4 @@ default['prometheus_exporters']['blackbox']['url'] = "https://github.com/prometh
 default['prometheus_exporters']['blackbox']['checksum'] = 'a2918a059023045cafb911272c88a9eb83cdac9a8a5e8e74844b5d6d27f19117'
 default['prometheus_exporters']['blackbox']['timeout_offset'] = '0.5'
 default['prometheus_exporters']['blackbox']['log_level'] = 'info'
+default['prometheus_exporters']['blackbox']['user'] = 'root'
