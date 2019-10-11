998167d5697e2684591b74671828bf5cecff0186
Stephan Scheying <stephan@scheying.de>
2019-10-11 16:33:04 +0200

    Make rubocop happy

--- recipes/varnish.rb
+++ recipes/varnish.rb
@@ -9,11 +9,11 @@
 
 unless node['prometheus_exporters']['disable']
   varnish_exporter 'main' do
-  	varnishstat node['prometheus_exporters']['varnish']['varnishstat']
-  	user node['prometheus_exporters']['varnish']['user']
-  	telemetry_address node['prometheus_exporters']['varnish']['telemetry_address']
-  	telemetry_endpoint node['prometheus_exporters']['varnish']['telemetry_endpoint']
-  	
+    varnishstat node['prometheus_exporters']['varnish']['varnishstat']
+    user node['prometheus_exporters']['varnish']['user']
+    telemetry_address node['prometheus_exporters']['varnish']['telemetry_address']
+    telemetry_endpoint node['prometheus_exporters']['varnish']['telemetry_endpoint']
+
     action %i[install enable start]
   end
 end
