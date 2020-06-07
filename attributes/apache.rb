1fe631a076bd07451e32c4598d8582937bf4b3f3
Shai Atias <shaiatias@users.noreply.github.com>
2020-06-07 13:04:22 +0300

    change download file to 0.8.0 version

--- attributes/apache.rb
+++ attributes/apache.rb
@@ -1,3 +1,3 @@
-default['prometheus_exporters']['apache']['version'] = '0.7.0'
+default['prometheus_exporters']['apache']['version'] = '0.8.0'
 default['prometheus_exporters']['apache']['url'] = "https://github.com/Lusitaniae/apache_exporter/releases/download/v#{node['prometheus_exporters']['apache']['version']}/apache_exporter-#{node['prometheus_exporters']['apache']['version']}.linux-amd64.tar.gz"
-default['prometheus_exporters']['apache']['checksum'] = '0258660d53fb745283ef8d50eef3e50a20c4899997df787b085d52d879bd0561'
+default['prometheus_exporters']['apache']['checksum'] = '8f952aa99b338a14a3c71591123c14c742cf238da83f3591879ad69f9d6b9080'
