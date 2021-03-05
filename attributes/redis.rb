d8648abcb411be99ec4744e1d691e613c2de11f1
Mario Faúndez Vidal <mariofaundez@hotmail.com>
2021-03-05 15:29:54 -0300

    updating prometheus_exporters to version: 1.17.1

--- attributes/redis.rb
+++ attributes/redis.rb
@@ -1,3 +1,3 @@
-default['prometheus_exporters']['redis']['version'] = '1.6.1'
+default['prometheus_exporters']['redis']['version'] = '1.17.1'
 default['prometheus_exporters']['redis']['url'] = "https://github.com/oliver006/redis_exporter/releases/download/v#{node['prometheus_exporters']['redis']['version']}/redis_exporter-v#{node['prometheus_exporters']['redis']['version']}.linux-amd64.tar.gz"
-default['prometheus_exporters']['redis']['checksum'] = '9e0e75bdf246a13807d1a205075a711721a7b065dd32e921b5f55536dbc63600'
+default['prometheus_exporters']['redis']['checksum'] = 'd424d0524867a1fc503e7335f0856d72f125a4331b759bc3c61cdcbbb80ae50e'
