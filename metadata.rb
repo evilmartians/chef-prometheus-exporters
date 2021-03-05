180c8cf3a4223b54b24d39f14dc82054bc4b9588
Mario Faúndez Vidal <mariofaundez@hotmail.com>
2021-03-05 15:29:54 -0300

    updating redis-exporter to version: 1.17.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'surrender@evilmartians.com'
 license          'Apache-2.0'
 description      'Installs / configures Prometheus exporters'
 
-version          '0.17.2'
+version          '0.17.3'
 
 chef_version '>= 12.11', '< 17.0'
 
